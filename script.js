5cd477b7ed6d41fe1eaebad744cc4b65b903f31f
Caitlin Bopp <boppcaitlin@gmail.com>
2021-03-09 19:14:52 -0500

    Added comments to javascript file

---
+++ script.js
@@ -0,0 +1,8 @@
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+
+var timer;
+var timerCount;
+
+// How to make an array of questions & correct answers?
+
