2208fc3f0c9fa9304fc6bde4ba9518ed919b51dc
Caitlin Bopp <boppcaitlin@gmail.com>
2021-03-12 23:00:11 -0500

    Updated quiz questions, added in additional comments to getNextQuestion function.

--- script.js
+++ script.js
@@ -5,18 +5,32 @@ var timer;
 var timerCount;
 var gameIndex = 0;
 
-// How to make an array of questions & correct answers?
-// this is our index
+// This is my index/array of questions & answers
 var questionsAnswers = [
     {
-        q: "Commonly used data types do NOT include:", 
-        choices: ["1. strings", "2. bulleons", "3. alerts", "4. numbers"],
-        answers: "3. alerts"
+        q: "Which is the largest state in the United States of America?", 
+        choices: ["1. Alaska", "2. California", "3. Texas", "4. Washington"],
+        answers: "1. Alaska"
     },
     {
-        q: "What is my favorite color?", 
-        choices: ["choice a", "choice b", "choice c", "choice d"],
-        answers: "choice a"
+        q: "How many colors are in the rainbow?", 
+        choices: ["1. Five", "2. Ten", "3. Nine", "4. Seven"],
+        answers: "4. Seven"
+    },
+    {
+        q: "How many sides are there to an octogon?", 
+        choices: ["1. Four", "2. Eight", "3. Five", "4. Ten"],
+        answers: "2. Eight"
+    },
+    {
+        q: "How many dimes are in 1 dollar?", 
+        choices: ["1. 20", "2. 100", "3. 10", "4. 50"],
+        answers: "3. 10"
+    },
+    {
+        q: "What is the largest country in the world?", 
+        choices: ["1. Russia", "2. Canada", "3. China", "4. United States"],
+        answers: "1. Russia"
     }
 ]
 //console.log (questionsAnswers[1].q)
@@ -59,28 +73,28 @@ function getNextQuestion() {
     quizQuestion.textContent = questionsAnswers[gameIndex].q;
     for (var i = 0; i < questionsAnswers[gameIndex].choices.length;
         i++) {
-// Create element - creating a button dynamically in javascript
+// Create element/button
     var btn = document.createElement("button");
+// Inside button, add text content
     btn.textContent = questionsAnswers[gameIndex].choices[i];
+// then append button to front end
     document.getElementById("quizQuestion").appendChild(btn);
     }
-}  
-    
-    //     document.getElementById("choices").innerHTML = "<button>" + choices[i] + "</button>";
-    //var textnode = document.createTextNode("water");
 
-    
-    //btn.appendChild(textnode);
+} 
+    getNextQuestion();
 
-    
-
-    // document.p.appendChild(btn);
-// Inside button, add text content textContent.choices i
-// then append button to front end (section quiz in html)
-
-
-getNextQuestion();
-
+// When user clicks on incorrect answer, show "Wrong answer" & advance to next question
+// When user clicks on correct answer, show "Correct answer!" & advance to next question
+   
+// var userChoice = document.getElementById("userChoice");
+// btn.addEventListener("click", function(){
+//     if (btn.textContent = questionsAnswers[gameIndex].answers){
+//         document.getElementById("userChoice") = "Correct Answer!", getNextQuestion();
+//     } else {
+//         document.getElementById("userChoice") = "Wrong Answer!", getNextQuestion();
+//     }
+// });
 
 //  Attach event listener to start button to call startGame function on click
 startButton.addEventListener("click", startGame);
