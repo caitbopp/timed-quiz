27095a2b3a9a5caf16ffcee08a59cf64d41cdc92
Caitlin Bopp <boppcaitlin@gmail.com>
2021-03-14 21:47:23 -0400

    Corrected timer function and added end game screen and submitting score functionality.

--- script.js
+++ script.js
@@ -10,27 +10,27 @@ var questionsAnswers = [
     {
         q: "Commonly used data types DO NOT include:",
         choices: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
-        answers: "3. Alerts"
+        answer: "3. Alerts"
     },
     {
         q: "String values must be enclosed within _____ when being assigned to variables.",
         choices: ["1. Commas", "2. Curly Brackets", "3. Quotes", "4. Parentheses"],
-        answers: "3. Quotes"
+        answer: "3. Quotes"
     },
     {
         q: "Arrays in JavaScript can be used to store _____.",
         choices: ["1. Numbers and strings", "2. Other arrays", "3. Booleans", "4. All of the above"],
-        answers: "4. All of the above"
+        answer: "4. All of the above"
     },
     {
         q: "A very useful tool used during development and debugging for printing content to the debugger is:",
         choices: ["1. JavaScript", "2. Terminal/Bash", "3. For loops", "4. console.log"],
-        answers: "4. console.log"
+        answer: "4. console.log"
     },
     {
         q: "The condition in an if / else statement is enclosed within _____.",
         choices: ["1. Quotes", "2. Curly brackets", "3. Parentheses", "4. Square brackets"],
-        answers: "2. Curly brackets"
+        answer: "2. Curly brackets"
     }
 ]
 //console.log (questionsAnswers[1].q)
@@ -64,13 +64,17 @@ function startTimer() {
                 clearInterval(timer);
                 winGame();
             }
+            if (timerCount == -1) {
+                clearInterval(timer);
+                alert("Time is up!");
+            }
         }
         // if (timerCount = 0) {
         //     stopTimer()
         //     clearInterval(timer);
         // }
     }, 1000);
-}
+};
 
 // Function to call questionsAnswers
 function getNextQuestion() {
@@ -89,38 +93,78 @@ function getNextQuestion() {
             btn.textContent = questionsAnswers[gameIndex].choices[i];
             // then append button to front end
             document.getElementById("quizQuestion").appendChild(btn);
-            // }
-            //gameIndex++;
         }
     }
-
-    //getNextQuestion();
-
-    // When user clicks on incorrect answer, show "Wrong answer" & advance to next question
-    // When user clicks on correct answer, show "Correct answer!" & advance to next question
 };
 
+// function checkChoice() {
+//     if (questionsAnswers[gameIndex].answer === answerBtn.target.textContent) {
+//         timer += 20 - timerCount;
+//         alert("Correct!");
+//         score += 6;
+//         timerCount = 20;
+//     } else {
+//         timerCount += 20;
+//     }
+// };
+
+
+
+// When user clicks on incorrect answer, show "Wrong answer" & advance to next question
+// When user clicks on correct answer, show "Correct answer!" & advance to next question
 var quizSection = document.getElementById("quiz");
 quizSection.addEventListener("click", function (event) {
     var element = event.target;
     gameIndex++;
 
     if (element.matches("button")) {
-
-        if (element.textContent == questionsAnswers[gameIndex].answers) {
-            document.getElementById("userChoice") = "Correct Answer!";
+        if (questionsAnswers[gameIndex].answer === event.target.textContent) {
+            timer += 20 - timerCount;
+            alert("Correct!");
+            score += 6;
+            timerCount = 20;
         } else {
-            document.getElementById("userChoice") == "Wrong Answer!";
+            timerCount += 20;
         }
+        // if (element.textContent == questionsAnswers[gameIndex].answer) {
+        //     document.getElementById("userChoice") = "Correct Answer!";
+        // } else {
+        //     document.getElementById("userChoice") == "Wrong Answer!";
+        // }
         // need to check for additional questions before calling nextQuestion
         if (gameIndex <= 4) {
             getNextQuestion();
         } else {
-            scorePage();
+            gameOver();
+        }
+
+        if (timerCount <= 0) {
+            clearInterval(timer);
+            gameOver();
         }
     };
 
 });
+
+
+
+function gameOver() {
+    //isWin = false;
+    timerCount = 0;
+    startButton.disabled = true;
+
+    var welcome = document.getElementById("welcome");
+    welcome.classList.add("display");
+
+    var quiz = document.getElementById("quiz");
+    quiz.classList.add("display");
+
+    var finish = document.getElementById("finish");
+    finish.classList.remove("display");
+
+};
+
+
 
 
 //  Attach event listener to start button to call startGame function on click
